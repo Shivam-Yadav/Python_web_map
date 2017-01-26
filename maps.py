a22a04385e4e4e83774622c157781e472e3f9bb9
Shivam-Yadav <shivam.yadav@st.niituniversity.in>
2017-01-26 18:29:48 +0530

    using max n min

--- maps.py
+++ maps.py
@@ -4,14 +4,28 @@ import pandas        #import pandas lib for reading data off files
 
 df=pandas.read_csv('Volcanoes-USA.txt')   #create a dataframe object
 
-map = folium.Map(location=[45.372, -121.697], zoom_start=4, tiles='Stamen Terrain')  # create map
+map = folium.Map(location=[df['LAT'].mean(), df['LON'].mean()], zoom_start=4, tiles='Stamen Terrain')  # create map
 
-def color(elev):
+'''def color(elev):
      if elev in range(0,1000):
          col='green'
      elif elev in range(1000,3000):
          col='yellow'
      elif elev in range(3000,4000):
+         col='red'
+     else:
+         col='black'
+     return col
+'''
+def color(elev):
+     minimum=min(df['ELEV'])
+     maximum=max(df['ELEV'])
+     step = (maximum-minimum)/4
+     if elev in range(int(minimum) , int (minimum+step)):
+         col='green'
+     elif elev in range(int (minimum+step),int (minimum+step*2)):
+         col='blue'
+     elif elev in range(int (minimum+step*2), int (minimum+step*3)):
          col='red'
      else:
          col='black'
